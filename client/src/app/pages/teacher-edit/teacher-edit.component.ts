a27ac2d92831365654776fea49f1712ef95d7d23
antoniovela219.av@gmail.com <antoniovela219.av@gmail.com>
2021-02-19 01:48:31 +0000

    File generated by Skaffolder - user: antoniovela219.av@gmail.com

---
+++ client/src/app/pages/teacher-edit/teacher-edit.component.ts
@@ -0,0 +1,127 @@
+// Import Libraries
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+// Import Services
+import { TeacherService } from '../../services/teacher.service';
+import { CourseService } from '../../services/course.service';
+// Import Models
+import { Teacher } from '../../domain/sms_db/teacher';
+import { Course } from '../../domain/sms_db/course';
+
+// START - USED SERVICES
+/**
+* teacherService.create
+*	@description CRUD ACTION create
+*
+* teacherService.update
+*	@description CRUD ACTION update
+*	@param ObjectId id Id
+*
+* teacherService.get
+*	@description CRUD ACTION get
+*	@param ObjectId id Id resource
+*
+* courseService.list
+*	@description CRUD ACTION list
+*
+*/
+// END - USED SERVICES
+
+/**
+ * This component allows to edit a Teacher
+ */
+@Component({
+    selector: 'app-teacher-edit',
+    templateUrl: 'teacher-edit.component.html',
+    styleUrls: ['teacher-edit.component.css']
+})
+export class TeacherEditComponent implements OnInit {
+    item: Teacher;
+    list_courses: Course[];
+    model: Teacher;
+    formValid: Boolean;
+
+    constructor(
+    private teacherService: TeacherService,
+    private courseService: CourseService,
+    private route: ActivatedRoute,
+    private location: Location) {
+        // Init item
+        this.item = new Teacher();
+    }
+
+    /**
+     * Init
+     */
+    ngOnInit() {
+        this.route.params.subscribe(param => {
+            const id: string = param['id'];
+            if (id !== 'new') {
+                this.teacherService.get(id).subscribe(item => this.item = item);
+            }
+            // Get relations
+            this.courseService.list().subscribe(list => this.list_courses = list);
+        });
+    }
+
+    /**
+     * Check if an Course is in  _courses
+     *
+     * @param {string} id Id of Course to search
+     * @returns {boolean} True if it is found
+     */
+    containCourse(id: string): boolean {
+        if (!this.item._courses) return false;
+        return this.item._courses.indexOf(id) !== -1;
+    }
+
+    /**
+     * Add Course from Teacher
+     *
+     * @param {string} id Id of Course to add in this.item._courses array
+     */
+    addCourse(id: string) {
+        if (!this.item._courses)
+            this.item._courses = [];
+        this.item._courses.push(id);
+    }
+
+    /**
+     * Remove an Course from a Teacher
+     *
+     * @param {number} index Index of Course in this.item._courses array
+     */
+    removeCourse(index: number) {
+        this.item._courses.splice(index, 1);
+    }
+
+    /**
+     * Save Teacher
+     *
+     * @param {boolean} formValid Form validity check
+     * @param Teacher item Teacher to save
+     */
+    save(formValid: boolean, item: Teacher): void {
+        this.formValid = formValid;
+        if (formValid) {
+            if (item._id) {
+                this.teacherService.update(item).subscribe(data => this.goBack());
+            } else {
+                this.teacherService.create(item).subscribe(data => this.goBack());
+            } 
+        }
+    }
+
+    /**
+     * Go Back
+     */
+    goBack(): void {
+        this.location.back();
+    }
+
+
+}
+
+
+
